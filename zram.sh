5f15decd6d04f41d9c61b02ed373003caf8117b0
RahulHackz <41478337+RahulHackz@users.noreply.github.com>
2019-09-01 14:33:15 +0530

    Add files via upload

---
+++ zram.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+echo "Made By RahulHackz With Love"
+cores=$(nproc --all)
+modprobe zram num_devices=$cores
+
+swapoff -a
+
+totalmem=`free | grep -e "^Mem:" | awk '{print $2}'`
+mem=$(( ($totalmem / $cores)* 1024 ))
+
+core=0
+while [ $core -lt $cores ]; do
+  echo $mem > /sys/block/zram$core/disksize
+  mkswap /dev/zram$core
+  swapon -p 5 /dev/zram$core
+  let core=core+1
+done
